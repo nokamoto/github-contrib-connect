80f8bb4d5bc54db79f37fc7f3b63b1df84f3ff74
Naofumi Okamoto <nokamoto.engr@gmail.com>
2023-09-07 10:08:40 +0900

    Add infra Github (#22)

--- cmd/apis/main.go
+++ cmd/apis/main.go
@@ -1,3 +1,28 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/nokamoto/github-contrib-connect/internal/infra"
+)
+
+func main() {
+	client := infra.NewClient(os.Getenv("GITHUB_TOKEN"), 1*time.Second)
+	ctx := context.Background()
+
+	repos, err := client.ListRepositories(ctx, "golang")
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < 1; i++ {
+		pulls, err := client.ListPulls(ctx, "golang", repos[i].GetName())
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("%s/%s: %d\n", repos[i].GetOwner().GetLogin(), repos[i].GetName(), len(pulls))
+	}
+}
